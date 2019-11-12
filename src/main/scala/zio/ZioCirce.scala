8f101465c423056126d6012efe7db0d896de80e2
Loïc Descotte <loic.descotte@kelkoo.com>
2019-11-12 10:52:31 +0100

    use environment to pass the STTP backend

--- src/main/scala/zio/ZioCirce.scala
+++ src/main/scala/zio/ZioCirce.scala
@@ -16,23 +16,37 @@ case class Message(text: String, author: String)
 
 object MixedStream extends App {
 
-  type ZioSttpBackend = SttpBackend[Task,Stream[Throwable, ByteBuffer],WebSocketHandler]
+  type ZioSttpBackend = SttpBackend[Task, Stream[Throwable, ByteBuffer], WebSocketHandler]
 
-  def queryToStream(keyword: String)(implicit sttpBackend: ZioSttpBackend): Task[ZStream[Any, Throwable, Message]] = {
-    val responseIO =
-      basicRequest
-        .post(uri"http://localhost:3000?keyword=$keyword")
-        .response(asStream[Stream[Throwable, ByteBuffer]])
-        .readTimeout(Duration.Inf)
-        .send()
+  trait MixedStreamModule extends Console {
+    def sttpBackend[R]: RIO[R, ZioSttpBackend]
+  }
 
-    responseIO.map { response =>
-      response.body match {
-        case Right(stream) =>
-          delimitByLine(stream).map { line =>
-            decode[Message](line).getOrElse(Message("Error parsing JSON", "app"))
-          }
-        case Left(_) => Stream(Message("http error", "app"))
+  // live module
+  // for test you can define a test module object with a stubbed sttp backend (see https://sttp.readthedocs.io/en/latest/testing.html)
+  object MixedStreamModuleLive extends MixedStreamModule with Console.Live {
+    def sttpBackend[R]: RIO[R, ZioSttpBackend] = ZIO.runtime.flatMap((r: Runtime[R]) => AsyncHttpClientZioStreamsBackend[R](r))
+  }
+
+  def queryToStream(keyword: String): RIO[MixedStreamModule, Stream[Throwable, Message]] = {
+
+    ZIO.accessM[MixedStreamModule](_.sttpBackend).flatMap { implicit backend =>
+
+      val responseIO =
+        basicRequest
+          .post(uri"http://localhost:3000?keyword=$keyword")
+          .response(asStream[Stream[Throwable, ByteBuffer]])
+          .readTimeout(Duration.Inf)
+          .send()
+
+      responseIO.map { response =>
+        response.body match {
+          case Right(stream) =>
+            delimitByLine(stream).map { line =>
+              decode[Message](line).getOrElse(Message("Error parsing JSON", "app"))
+            }
+          case Left(_) => Stream(Message("http error", "app"))
+        }
       }
     }
   }
@@ -45,15 +59,12 @@ object MixedStream extends App {
       .flatMap(chunk => Stream.fromChunk(chunk))
   }
 
-  def run(args: List[String]) = {
-    val sttpBackend: Task[ZioSttpBackend] = AsyncHttpClientZioStreamsBackend(this)
-    sttpBackend.flatMap { implicit backend =>
-      val streamsIO: Task[List[Stream[Throwable, Message]]] =
-        ZIO.foreach(List("ZIO", "FS2"))(queryToStream)
-      val mergedIO: Task[Stream[Throwable, Message]] =
-        streamsIO.map(streams => streams.reduceLeft(_.merge(_)))
-      mergedIO.flatMap(_.foreach(message => putStrLn(message.toString)))
-    }.fold(_ => 1, _ => 0)
+  val program: ZIO[MixedStreamModule, Throwable, Unit] = {
+    val streamsIO = ZIO.foreach(List("ZIO", "FS2"))(queryToStream)
+    val mergedIO = streamsIO.map(streams => streams.reduceLeft(_.merge(_)))
+    mergedIO.flatMap(_.foreach(message => putStrLn(message.toString)))
   }
+
+  def run(args: List[String]) = program.provide(MixedStreamModuleLive).fold(_ => 1, _ => 0)
 
 }
